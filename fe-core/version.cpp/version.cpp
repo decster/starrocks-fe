7546140235d3dfd190e29f6f30809759272d9df2
Binglin Chang <decstery@gmail.com>
2025-06-18 18:37:06 +0800

    initial commit

---
+++ fe-core/version.cpp/version.cpp
@@ -0,0 +1,29 @@
+
+// Copyright 2021-present StarRocks, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOTE: This is a generated file, DO NOT EDIT IT
+// FINGERPRINT: 42dc9ed78833dcefc7672dd3df931a58
+
+namespace starrocks {
+
+const char* STARROCKS_VERSION = "UNKNOWN";
+const char* STARROCKS_COMMIT_HASH = "UNKNOWN";
+const char* STARROCKS_BUILD_TIME = "2025-06-18 18:24:06";
+const char* STARROCKS_BUILD_USER = "decster";
+const char* STARROCKS_BUILD_HOST = "decster-MS-7C94 (Ubuntu 24.04.2 LTS)";
+const char* STARROCKS_BUILD_DISTRO_ID = "ubuntu";
+const char* STARROCKS_BUILD_ARCH = "x86_64";
+}
+
