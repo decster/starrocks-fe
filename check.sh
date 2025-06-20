4c4b55dcd1da6b2e9ab85886de0b104918900451
Binglin Chang <decstery@gmail.com>
2025-06-20 10:34:37 +0800

    make project structure more link original

--- check.sh
+++ check.sh
@@ -1,2 +1,4 @@
+pushd fe
 mvn compile -Dcheckstyle.skip=true -DskipTests=true | grep -v -E '^(Downloading|Downloaded|Progress|^\[INFO\] Downloading|^\[INFO\] Downloaded)'
+popd
 exit ${PIPESTATUS[0]}
